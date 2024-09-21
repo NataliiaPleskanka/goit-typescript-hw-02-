96973624e0a606c77996cd6108141bc85853d312
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-09-21 21:28:11 +0200

    loader

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default async function getPhotos(value, page = 1) {
+  const URL = "https://api.unsplash.com/search/photos";
+  const params = {
+    client_id: "FAI36XVEz-8UQIKzygO5eEXvTTH7feuP-GzMaCSBIjw",
+    query: value,
+    per_page: 3,
+    page,
+  };
+
+  return await axios.get(URL, { params });
+}
